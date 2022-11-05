f07f8fecc3d7a7365561e23e1bbcd1094bce94c1
DarkValkier <darkvalkier@gmail.com>
2022-11-05 10:15:16 +0200

    Сделан бой насмерть

--- Lesson_3/main.py
+++ Lesson_3/main.py
@@ -6,8 +6,10 @@ player2 = Character(name='Petya', damage=8)
 print(player1, '\n')
 print(player2, '\n')
 
-player1.attack(player2)
-player2.attack(player1)
 
-print(player1, '\n')
-print(player2, '\n')
+while player1.is_alive() and player2.is_alive():
+    player1.attack(player2)
+    player2.attack(player1)
+
+    print(player1, '\n')
+    print(player2, '\n')
